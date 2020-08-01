b57167068ca7f4492d44f36111efc0e026fd5ce4
Jay Adamo <jadamo1976@gmail.com>
2020-08-01 16:31:22 -0400

    Adding initial successful building version of the package

---
+++ lambdata_jayadamo/__init__.py
@@ -0,0 +1,11 @@
+"""
+lambdata - a collection of Data Science helper functions
+"""
+import pandas as pd
+import numpy as np
+from . import example_module 
+
+# Writing functions to run
+# Defining TEST and Y
+TEST = pd.DataFrame(np.ones(10))
+Y = example_module.increment(example_module.x)
