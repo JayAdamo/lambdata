aaa25dccf0a448d4dd4711d0e0d58a5010c3a9ed
Jay Adamo <jadamo1976@gmail.com>
2020-08-03 16:33:20 -0400

    Added code and helper functions to dataframe_helper.py

--- lambdata_jayadamo/dataframe_helper.py
+++ lambdata_jayadamo/dataframe_helper.py
@@ -3,7 +3,36 @@
 """
 Some functions to help cleaning and handling dataframes.
 """
+
+# import libraries
 import pandas as pd
+import random
+
+
+''' 1) A function to find missing values in a dataframe '''
+
+# defining report_missing_values
 def report_missing_values(df):
-    """Print a pretty report of missing values."""
-    print('Need to write code here!')
+    pd.isnull = missing_values
+    
+    # Show missing values
+    print(f'Here are the missing values:', missing_values)
+
+
+''' 2) A function to add 2 numbers by user input '''
+
+# Define 2 possible values
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+''' 3) Function to generate a random number between 0 and 9 '''
+
+# Shows random number
+print(random.randint(0,9))
